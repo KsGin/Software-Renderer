179fff754d6dc989ace6479d3d19fb49281f6c1e
KsGin <imqqyangfan@gmail.com>
2018-05-04 16:05:50 +0800

    Update : 2018.5.4 by ksgin

--- js/device.js
+++ js/device.js
@@ -34,45 +34,59 @@ Device.prototype.putPixel = function (x, y, z, color) {
     this.backbufferdata[index + 2] = color.b * 255;
     this.backbufferdata[index + 3] = color.a * 255;
 };
-Device.prototype.project = function (coord, transMat) {
-    let point = Vector3.TransformCoordinates(coord, transMat);
-    let x = -point.x * this.workingWidth + this.workingWidth / 2.0 >> 0;
-    let y = -point.y * this.workingHeight + this.workingHeight / 2.0 >> 0;
-    return (new Vector3(x, y, point.z));
+
+Device.prototype.project = function (vertex, transMat, worldMat) {
+    let position2D = Vector3.TransformCoordinates(vertex.position, transMat);
+
+    let x = -position2D.x * this.workingWidth + this.workingWidth / 2.0 >> 0;
+    let y = -position2D.y * this.workingHeight + this.workingHeight / 2.0 >> 0;
+
+    let position3D = Vector3.TransformCoordinates(vertex.position, worldMat);
+    let normal3D = Vector3.TransformNormal(vertex.normal, worldMat);
+
+    return ({
+        position2D : new Vector3(x , y , position2D.z) ,
+        position3D : position3D ,
+        normal3D : normal3D ,
+        texcoord : vertex.texcoord
+    });
 };
+
 Device.prototype.drawPoint = function (point, color) {
     if (point.x >= 0 && point.y >= 0 && point.x < this.workingWidth && point.y < this.workingHeight) {
         this.putPixel(point.x, point.y, point.z, color);
     }
 };
 
-Device.prototype.render = function (camera, model , worldMatrix, viewMatrix, projectionMatrix) {
+Device.prototype.render = function (camera, model, worldMatrix, viewMatrix, projectionMatrix) {
     let transformMatrix = worldMatrix.multiply(viewMatrix).multiply(projectionMatrix);
     model.meshes.forEach(mesh => {
         let idx = 0;
         mesh.Faces.forEach(face => {
-            let p1 = this.project(mesh.Vertices[face.A].position, transformMatrix);
-            let p2 = this.project(mesh.Vertices[face.B].position, transformMatrix);
-            let p3 = this.project(mesh.Vertices[face.C].position, transformMatrix);
+            let v1 = this.project(mesh.Vertices[face.A], transformMatrix, worldMatrix);
+            let v2 = this.project(mesh.Vertices[face.B], transformMatrix, worldMatrix);
+            let v3 = this.project(mesh.Vertices[face.C], transformMatrix, worldMatrix);
+
             let color = 0.25 + ((idx % mesh.Faces.length) / mesh.Faces.length) * 0.75;
             let finalColor = new Color4(color, color, 0, 1);
+
             if (this.isWireFrame) {
-                this.drawLine(p1, p2, finalColor);
-                this.drawLine(p2, p3, finalColor);
-                this.drawLine(p3, p1, finalColor);
+                this.drawLine(v1, v2, finalColor);
+                this.drawLine(v2, v3, finalColor);
+                this.drawLine(v3, v1, finalColor);
             } else {
-                this.drawTriangle(p1, p2, p3, finalColor);
+                this.drawTriangle(v1, v2, v3, finalColor);
             }
             idx++;
         });
     });
 };
 
-Device.prototype.drawLine = function (point0, point1, color) {
-    let x0 = point0.x >> 0;
-    let y0 = point0.y >> 0;
-    let x1 = point1.x >> 0;
-    let y1 = point1.y >> 0;
+Device.prototype.drawLine = function (v0, v1, color) {
+    let x0 = v0.position2D.x >> 0;
+    let y0 = v0.position2D.y >> 0;
+    let x1 = v1.position2D.x >> 0;
+    let y1 = v1.position2D.y >> 0;
     let dx = Math.abs(x1 - x0);
     let dy = Math.abs(y1 - y0);
     let sx = (x0 < x1) ? 1 : -1;
@@ -109,7 +123,12 @@ Device.prototype.interpolate = function (min, max, gradient) {
     return min + (max - min) * this.clamp(gradient);
 };
 
-Device.prototype.processScanLine = function (y, pa, pb, pc, pd, color) {
+Device.prototype.processScanLine = function (y, va, vb, vc, vd, color) {
+
+    let pa = va.position2D;
+    let pb = vb.position2D;
+    let pc = vc.position2D;
+    let pd = vd.position2D;
 
     let gradient1 = (pa.y !== pb.y) ? (y - pa.y) / (pb.y - pa.y) : 1;
     let gradient2 = (pc.y !== pd.y) ? (y - pc.y) / (pd.y - pc.y) : 1;
@@ -136,19 +155,24 @@ Device.prototype.processScanLine = function (y, pa, pb, pc, pd, color) {
 };
 
 Device.prototype.drawTriangle = function (p1, p2, p3, color) {
+
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+
     let y;
     let temp;
-    if (p1.y > p2.y) {
+    if (p1.position2D.y > p2.position2D.y) {
         temp = p2;
         p2 = p1;
         p1 = temp;
     }
-    if (p2.y > p3.y) {
+    if (p2.position2D.y > p3.position2D.y) {
         temp = p2;
         p2 = p3;
         p3 = temp;
     }
-    if (p1.y > p2.y) {
+    if (p1.position2D.y > p2.position2D.y) {
         temp = p2;
         p2 = p1;
         p1 = temp;
@@ -158,29 +182,29 @@ Device.prototype.drawTriangle = function (p1, p2, p3, color) {
     let dP1P2;
     let dP1P3;
 
-    if (p2.y - p1.y > 0) {
-        dP1P2 = (p2.x - p1.x) / (p2.y - p1.y);
+    if (p2.position2D.y - p1.position2D.y > 0) {
+        dP1P2 = (p2.position2D.x - p1.position2D.x) / (p2.position2D.y - p1.position2D.y);
     } else {
         dP1P2 = 0;
     }
 
-    if (p3.y - p1.y > 0) {
-        dP1P3 = (p3.x - p1.x) / (p3.y - p1.y);
+    if (p3.position2D.y - p1.position2D.y > 0) {
+        dP1P3 = (p3.position2D.x - p1.position2D.x) / (p3.position2D.y - p1.position2D.y);
     } else {
         dP1P3 = 0;
     }
 
     if (dP1P2 > dP1P3) {
-        for (y = p1.y >> 0; y <= p3.y >> 0; y++) {
-            if (y < p2.y) {
+        for (y = p1.position2D.y >> 0; y <= p3.position2D.y >> 0; y++) {
+            if (y < p2.position2D.y) {
                 this.processScanLine(y, p1, p3, p1, p2, color);
             } else {
                 this.processScanLine(y, p1, p3, p2, p3, color);
             }
         }
     } else {
-        for (y = p1.y >> 0; y <= p3.y >> 0; y++) {
-            if (y < p2.y) {
+        for (y = p1.position2D.y >> 0; y <= p3.position2D.y >> 0; y++) {
+            if (y < p2.position2D.y) {
                 this.processScanLine(y, p1, p2, p1, p3, color);
             } else {
                 this.processScanLine(y, p2, p3, p1, p3, color);
